36a4717af8558a2c87d8e353a30652ef06156235
Rupesh Deshmukh <rupesh.deshmukh@atos.net>
2018-09-26 11:51:07 +0530

    Renamed files.

--- techhub-web/env.bat
+++ techhub-web/env.bat
@@ -1,7 +1,3 @@
-set MYSQL_JDBC_URL=jdbc:mysql://localhost:3306/wlodc_techhub
+set MYSQL_JDBC_URL=jdbc:mysql://localhost:3306/techhub
 set MYSQL_USER=root
 set MYSQL_PASSWORD=root
-
-rem set MYSQL_JDBC_URL=jdbc:mysql://wlodc-techhub.cao530ol5djp.ap-south-1.rds.amazonaws.com:3306/wlodc_techhub
-rem set MYSQL_USER=wlodc_techhub
-rem set MYSQL_PASSWORD=SU_WLODC_2809
